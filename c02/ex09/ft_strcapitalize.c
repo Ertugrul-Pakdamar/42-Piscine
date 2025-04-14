3cbac6cc60742355e9ab670144e1dcb27b45d952
Ertuğrul Pakdamar <83445911+Ertugrul-Pakdamar@users.noreply.github.com>
2025-04-14 21:00:52 +0300

    c02 last

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -12,3 +12,56 @@
 
 #include <unistd.h>
 
+int	is_alpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int	is_alpha_or_num(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+char	to_upper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+char	to_lower(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	index;
+	int	is_new_word;
+
+	index = 0;
+	is_new_word = 1;
+	while (str[index])
+	{
+		if (is_alpha_or_num(str[index]) == 1)
+		{
+			if (is_new_word == 1 && is_alpha(str[index]) == 1)
+				str[index] = to_upper(str[index]);
+			else
+				str[index] = to_lower(str[index]);
+			is_new_word = 0;
+		}
+		else
+			is_new_word = 1;
+		index++;
+	}
+	return (str);
+}
---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 20:33:30 by epakdama          #+#    #+#             */
+/*   Updated: 2025/04/14 20:33:30 by epakdama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	index;
+	unsigned int	len;
+
+	index = 0;
+	len = 0;
+	while (src[len])
+		len++;
+	while (index <= size - 1)
+	{
+		if (size < len && index == size - 1)
+		{
+			dest[index] = '\0';
+		}
+		else
+			dest[index] = src[index];
+		index++;
+	}
+	return (len);
+}
