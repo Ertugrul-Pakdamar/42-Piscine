e40e57f67435fb66d89778a5909abe1a6b7cc36e
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-04-22 21:01:03 +0300

    c08ex01

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 19:40:10 by epakdama          #+#    #+#             */
+/*   Updated: 2025/04/22 20:58:37 by epakdama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN(nbr) (((nbr) % 2 == 0) ? 1 : 0)
+
+typedef int	t_bool;
+
+#endif
