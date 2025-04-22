297bb98d4b5b36d6dc738f47723d9c661adb387e
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-04-22 21:15:37 +0300

    c08ex03

--- C08/ex03/ft_point.h
+++ C08/ex03/ft_point.h
@@ -1,18 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_abs.h                                           :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/22 21:01:29 by epakdama          #+#    #+#             */
-/*   Updated: 2025/04/22 21:05:28 by epakdama         ###   ########.fr       */
+/*   Created: 2025/04/22 21:05:52 by epakdama          #+#    #+#             */
+/*   Updated: 2025/04/22 21:14:06 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_ABS_H
-# define FT_ABS_H
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
-# define ABS(nb) ((nb < 0) ? -nb : nb)
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	set_point(t_point *point);
 
 #endif
