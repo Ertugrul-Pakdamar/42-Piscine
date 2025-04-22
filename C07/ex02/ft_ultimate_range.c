e6866756f9f202e081db448ae7d9de5fd80c616f
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-04-22 17:27:35 +0300

    c07ex02

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 17:15:37 by epakdama          #+#    #+#             */
+/*   Updated: 2025/04/22 17:27:10 by epakdama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	*arr;
+	int	index;
+
+	if (min >= max)
+	{
+		*range = 0;
+		return (0);
+	}
+	arr = (int *)malloc(sizeof(int) * (max - min));
+	if (!arr)
+		return (-1);
+	index = 0;
+	while (index < max - min)
+	{
+		arr[index] = min + index;
+		index++;
+	}
+	*range = arr;
+	return (max - min);
+}
