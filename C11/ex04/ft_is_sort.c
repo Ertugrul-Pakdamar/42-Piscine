38aaffee13e7f129365ea262b788204b3dcffccc
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-04-29 20:18:34 +0300

    c11last

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:32:13 by epakdama          #+#    #+#             */
-/*   Updated: 2025/04/28 12:45:04 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/04/29 20:18:17 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,11 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 	if (!tab[1])
 		return (1);
 	index = 1;
-	while (tab[index])
+	while (index < length)
 	{
-		if (!is_positive(f(tab[index - 1], tab[index])))
+		if (is_positive(f(tab[index - 1], tab[index])))
 			return (0);
 		index++;
 	}
 	return (1);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	int	tab[4] = {1, 2, 3};
 }
